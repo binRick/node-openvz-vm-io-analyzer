2d9d52cdd63244083e46f0f6f27d28d01def971e
Rick <rick@Ricks-MacBook-Air.local>
2015-08-06 12:33:59 -0600

    iiiwq

--- client.js
+++ client.js
@@ -1,15 +1,13 @@
 var vzStat = require('./'),
+    clear = require('cli-clear'),
+    pj = require('prettyjson'),
     pb = require('pretty-bytes'),
     _ = require('underscore'),
     speedometer = require('speedometer');
 
-
-
-
-
 var Freq = 5000;
-
 var VMs = {};
+var debug = true;
 
 setInterval(function() {
     vzStat.collectReads({}, function(e, reads) {
@@ -20,26 +18,35 @@ setInterval(function() {
                     ctid: read.ctid,
                     updates: 0,
                     reads: 0,
-                    readsPerSecond: 0,
                     totalReads: 0,
                     startReads: read.reads,
                     speed: new speedometer(Freq / 1000),
-                    curSpeed: 0,
+                    curRate: 0,
+		    curRatePretty: '',
                     rates: []
                 };
 
             else {
                 VMs[read.ctid].updates++;
-                VMs[read.ctid].curSpeed = VMs[read.ctid].speed(read.reads - VMs[read.ctid].startReads);
+                VMs[read.ctid].curRate = VMs[read.ctid].speed(read.reads - VMs[read.ctid].startReads);
+		VMs[read.ctid].curRatePretty = pb(Math.round(VMs[read.ctid].curRate,0)) + '/sec';
+		VMs[read.ctid].Frequency = Freq;
                 VMs[read.ctid].rates.push({
                     ts: new Date().getTime(),
-                    rate: VMs[read.ctid].curSpeed
+                    rate: VMs[read.ctid].curRate
                 });
             }
         });
+
+VMs = _.toArray(VMs);
+
+
         _.each(VMs, function(vm) {
-            if (vm.curSpeed > 0)
-                console.log(vm.ctid, pb(Math.round(vm.curSpeed, 0)));
+	if(debug){
+            if (vm && vm.curRate && vm.curRate > 0)
+                console.log(vm.ctid, vm.curRatePretty);
+	//	console.log(pj.render(vm));
+	}
         });
     });
 }, Freq);
--- index.js
+++ index.js
@@ -9,8 +9,8 @@ child.execSync = child.execSync || require('exec-sync');
 module.exports.collectReads = function(Options, _cb) {
 
     var cmd = 'grep read /proc/bc/*/ioacct|  grep ^/proc/bc/[0-9]*.[0-9]/| sed -e \'s/\\// /g\' | sed -e \'s/[[:space:]]\\+/ /g\'| cut -d\' \' -f4,7| uniq | sort -k2 -n';
-//    var preCmd = 'ssh beo ssh corvus ';
-//    cmd = preCmd + '\"' + cmd + '\"';
+    var preCmd = 'ssh beo ssh corvus ';
+    cmd = preCmd + '\"' + cmd + '\"';
     var out = child.execSync(cmd).toString().split('\n').filter(function(s) {
         return s;
     }).map(function(s) {
